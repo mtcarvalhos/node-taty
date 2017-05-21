2de4fc48ed488dc626983e5d6c8bf84728143c8c
BlackBeard <ccarvalhos@outlook.com>
2017-05-21 15:13:11 -0300

    Version 1

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "MONGODB": process.env.MONGODB_URL,//|| 'mongodb://127.0.0.1:27017/taty',
     "MONGOLAB": 'mongodb://jmajor:sup0rt&ftp@ds125481.mlab.com:25481/moviesapp',
     "MONGODB_DATABASE": 'taty',
-    "SECRET": "thiswebpageisawesomejsforlife"
+    "SECRET": ""
 }
